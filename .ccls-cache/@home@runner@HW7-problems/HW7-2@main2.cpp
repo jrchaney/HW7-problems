0f196a296083350d006fabbd93c94981ac33d52b
jrchaney <111770024+jrchaney@users.noreply.github.com>
2022-10-07 18:44:16 +0000

    Initial commit

---
+++ .ccls-cache/@home@runner@HW7-problems/HW7-2@main2.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int time(int& secs);
+
+int main(){
+  int secs = 0;
+  cin >> secs;
+  
+}
+
+int time(int& secs){
+  
+}
---
+++ HW7-2/main2.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+
+using namespace std;
+
+void time(int& seconds);
+
+int main(){
+  int seconds = 0;
+  time(seconds);
+    
+}
+
+void time(int& seconds){
+
+  int hours;
+  int mins;
+  int secs;
+  cin >> seconds;
+  
+  if (seconds >= 3600)
+    hours = seconds/3600;
+  else
+    hours = 0;
+
+  if (seconds % 3600 == 0)
+    mins = 0;
+  else
+    mins = (seconds % 3600) / 60;
+
+  if ((seconds % 3600) % 60 == 0)
+    secs = 0;
+  else 
+    secs = (seconds % 3600) % 60;
+
+  cout << "The time is " << hours << " hour(s) " << endl;
+  cout << mins << " minute(s) " << endl;
+  cout << secs << " second(s) " << endl;  
+  }
